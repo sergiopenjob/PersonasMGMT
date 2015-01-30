59705f7e50f766652be380d1fb4376a62b9d28ea
Sergi Ohm <sergiout@hotmail.es>
2015-01-30 13:47:03 +0100

    Set edad para estudiante en el constructor

--- PersonasMGMT/src/personasmgmt/Estudiante.java
+++ PersonasMGMT/src/personasmgmt/Estudiante.java
@@ -8,7 +8,21 @@ public class Estudiante extends Persona{
         this.setEstudios(estudios);
     }
 
+    public Estudiante(String dni, String nombre, String estudios, int edad) {
+        this(dni, nombre, estudios);
+        this.setEdad(edad);
+    }
+    
     public void setEstudios(String estudios) {
         this.estudios = estudios;
+    }
+    
+    @Override
+    public void setEdad(int edad){
+        try {
+            super.setEdad(edad);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
